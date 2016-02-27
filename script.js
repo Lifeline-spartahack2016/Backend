55720ba8ffb00869621809fb169b7bea11132b3b
vivekcalambur <calambu2@illinois.edu>
2016-02-27 10:52:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ var connection = new Connection(config);
 connection.on('connect', function(err) {
 // If no error, then good to proceed.
     console.log("Connected");
-    executeStatement();
+    CreateNewUser();
 });
 
 // Step3
 var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 
-function executeStatement() {
-    request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {
+function CreateNewUser() {
+    request = new Request("INSERT INTO User_Info (Name, Email, Password\n VALUES(name, email, password)", function(err) {
     if (err) {
         console.log(err);}
     });
